c210477c90253a8f61c3173f1cdced158414ed25
Apollo <a5347354@gmail.com>
2016-08-04 23:53:28 +0800

    Finish but some bug was not solved

--- MapKit0804/MapKit0804/ViewController.swift
+++ MapKit0804/MapKit0804/ViewController.swift
@@ -21,7 +21,10 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, 
     var drawing: Bool = false //避免多個繪圖動作同時運行所需的旗標，因為為非同步的程式，避免在網路不順時出現兩條線
     var overlay: MKPolyline? //目前導航路線所在的overlay（蓋在地圖上的圖層）
     
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
@@ -41,12 +44,96 @@ class ViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, 
         mapView.showsUserLocation = true
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //每次更新Location時會呼叫
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        print(userLocation.coordinate) //座標資訊
+        //設定中心點為目前的座標
+        mapView.setCenterCoordinate(userLocation.coordinate, animated: true)
+        
+        //宣告一個可視範圍，單位公尺
+        let viewRegion = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 500, 500)
+        
+        //設定可視範圍,是否要動畫
+        mapView.setRegion(viewRegion, animated: true)
+        
+        drawRoute()
+        
+    }
+    
+    
+    
+    //小鍵盤右下角那顆
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        CLGeocoder().geocodeAddressString(searchbar.text!){ (placemarks, error) in
+            if let coordinate = placemarks?.first?.location?.coordinate {
+                self.dest = coordinate
+            
+                //TODO: 更換目的時重新繪製路線
+            }
+            
+        }
+    }
 
-
+    //有新的overlay就會呼叫這個方法
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        let render  = MKPolylineRenderer(polyline: self.overlay!)
+        render.strokeColor = UIColor(red: 0.9861, green: 0.2939, blue: 0.1098, alpha: 1.0) //設定顏色
+        render.lineWidth = 3 //設定粗細
+        return render
+    }
+    
+    
+    func drawRoute() {
+        
+        //如果正在畫，就return
+        if(drawing) {
+            return
+        }
+        
+        //guard 類似 if，只要掉進去一定要中斷
+        guard let dest = self.dest else {
+            return
+        }
+        
+        //要路線圖回來
+        let req = MKDirectionsRequest()
+        
+        //設定交通類型
+        req.transportType = .Automobile
+        
+        //出發地
+        req.source = MKMapItem(placemark: MKPlacemark(coordinate: mapView.userLocation.coordinate, addressDictionary: nil))
+        //目的地
+        req.destination = MKMapItem(placemark: MKPlacemark(coordinate: mapView.userLocation.coordinate, addressDictionary: nil))
+        
+        let directions = MKDirections(request: req)
+        
+        drawing = true
+        //Closure
+        directions.calculateDirectionsWithCompletionHandler{(res, error) in
+            //規劃路線時會有很多條，只取第一條
+            if let route = res?.routes.first {
+                //如果圖層存在，就移除掉
+                if let ol = self.overlay {
+                    self.mapView.removeOverlay(ol)
+                }
+                
+                //增加圖層
+                self.overlay = route.polyline
+                self.mapView.addOverlay(route.polyline)
+                
+                self.distanceLabel.text = "\(route.distance/1000)km"
+                self.timeLabel.text = "\(route.expectedTravelTime/3600)hr"
+            }
+            self.drawing = false
+        }
+    }
 }
 
